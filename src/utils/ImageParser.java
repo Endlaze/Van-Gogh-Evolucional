dead59e02d33e622fb048f69a9dd705615713c32
asanchezg <andres.sanchez@pernixlabs.com>
2018-09-04 21:27:44 -0600

    Adding method to create an image from an integer matrix

--- src/utils/ImageParser.java
+++ src/utils/ImageParser.java
@@ -1,4 +1,22 @@
 package utils;
 
+import java.awt.image.BufferedImage;
+
 public class ImageParser {
+
+
+    public BufferedImage matrixToImage(int [][] imageMatrix){
+
+        int height = imageMatrix.length;
+        int width = imageMatrix[0].length;
+        
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for(int y=0; y<height; y++) {
+            for (int x = 0; x < width; x++) {
+                bufferedImage.setRGB(x, y, imageMatrix[y][x]);
+            }
+        }
+        return bufferedImage;
+    }
 }
