a1cf575a1d70c25db058036fa66afac52c7014fc
asanchezg <andres.sanchez@pernixlabs.com>
2018-09-04 23:22:19 -0600

    Adding population class and init method

---
+++ src/geneticAlgorithm/Population.java
@@ -0,0 +1,20 @@
+package geneticAlgorithm;
+
+import java.util.ArrayList;
+
+public class Population {
+
+
+    ArrayList<Individual> individuals = new ArrayList<>();
+
+    public Population(int populationSize){
+        initializePopulation(populationSize);
+    }
+
+    private void initializePopulation(int populationSize){
+
+        for(int i=0; i<populationSize; i++){
+            this.individuals.add(new Individual());
+        }
+    }
+}
